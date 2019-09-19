1e8453a1a1bb4eb607e59635045ba0204a67088d
Ethan Collins <ethantcollins98@gmail.com>
2019-09-19 15:48:11 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CharactersContainer from './components/CharactersContainer';
+import SearchForm from './components/SearchForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BASEURL = 'https://rickandmortyapi.com/api/'
+//     /character for all characters
+//     /character?name=' + SEARCHEDNAME    to get back characters searched by name
+
+export default class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/CharactersContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Character from './Character'
+
+export default class CharactersContainer extends Component {
+
+    render(){
+        return(
+            <div>
+               
+            </div>
+        )
+    }
+}
